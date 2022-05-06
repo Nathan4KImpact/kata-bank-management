27911ebb6d28f0477293c628210f7f1163273298
Nathan4KImpact <nathanaeltalla@hotmail.com>
2022-05-06 21:13:22 +0200

    US 2: In order to retrieve some or all of my savings, As a bank client, I want to make a withdrawal from my account

--- src/test/java/com/mss/bankmngmtsys/BankMngtTests.java
+++ src/test/java/com/mss/bankmngmtsys/BankMngtTests.java
@@ -18,7 +18,7 @@ public class BankMngtTests {
     static Client client;
     static Account account;
     static Transaction transaction1;
-    Transaction transaction2;
+    static Transaction transaction2;
     Transaction transaction3;
     Transaction transaction4 ;
     static String transactionsHistory;
@@ -28,9 +28,13 @@ public class BankMngtTests {
         bankService = new BankService();
 
         client = new Client(1L, "Raoul FONGANG");
-        account = new Account(1L, client.getCliId(), "FRXX XXXX XXXX 0001");
-        transaction1 = new Transaction(1L, BigDecimal.TEN, EnumTransactionType.DEPOSIT, account);
+        account = new Account(1L, client.getCliId(), "FRXXXXXXXXXXXXXX0001");
         transactionsHistory = "D:\\MesPROJETS\\Ingeniance\\devs\\kata-bank-management\\database\\transactionsHistory.json";
+
+        transaction1 = new Transaction(1L, BigDecimal.TEN, EnumTransactionType.DEPOSIT, account);
+
+        account.setBalance(BigDecimal.TEN);
+        transaction2 = new Transaction(1L, BigDecimal.valueOf(5), EnumTransactionType.WITHDRAWAL, account);
     }
 
     /**
@@ -41,6 +45,17 @@ public class BankMngtTests {
     public void testMakeDeposit(){
         BigDecimal expected = BigDecimal.TEN;
         BigDecimal result = bankService.makeTransactionIntoAccount(transaction1, EnumTransactionType.DEPOSIT, client);
+        Assertions.assertEquals(expected, result);
+    }
+
+    /**
+     * US 2: In order to retrieve some or all of my savings, As a bank client, I want to make a withdrawal from my account
+     */
+    // Test case 2  : we make a 1st Withdrawal of a value in an empty account
+    @Test
+    public void testMakeWithDrawal(){
+        BigDecimal expected = BigDecimal.valueOf(5);
+        BigDecimal result = bankService.makeTransactionIntoAccount(transaction2, EnumTransactionType.WITHDRAWAL, client);
         Assertions.assertEquals(expected, result);
     }
 
